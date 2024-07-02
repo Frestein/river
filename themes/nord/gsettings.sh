2d57c878333b9f410d98635e593480970668507d
Frestein <Frestein@tuta.io>
2024-07-02 16:21:39 +0500

    fix(gtk): remove gtk window buttons

--- themes/nord/gsettings.sh
+++ themes/nord/gsettings.sh
@@ -4,3 +4,4 @@ gsettings set org.gnome.desktop.interface icon-theme "Nordic-Folders"
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic-darker"
 gsettings set org.gnome.desktop.interface cursor-theme "Qogirr-Dark"
 gsettings set org.gnome.desktop.interface font-name "Sans 9"
+gsettings set org.gnome.desktop.wm.preferences button-layout "" # Remove window buttons
