f8c7ece01fdf14aef023373e907bbcc25b97738e
Frestein <Frestein@tuta.io>
2024-07-05 02:27:33 +0500

    fix(autostart): resolve issues with launching gparted and timeshift

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,11 +6,19 @@ dir="$HOME/.config/river"
 # Fix the non-working xdg-desktop-portal-gtk service
 systemctl --user import-environment
 
-# Lauch statusbar
-if [[ $(pidof yambar) ]]; then
-  pkill yambar
+# Launch polkit agent
+ if [[ ! $(pidof polkit-gnome-authentication-agent-1) ]]; then
+  /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+ fi
+
+# Launch mpd with playerctl mpd-mpris plugin
+if [[ ! $(pidof mpd) ]]; then
+  mpd &
+  wait $!
+  mpd-mpris &
+elif [[ ! $(pidof mpd-mpris) ]]; then
+  mpd-mpris &
 fi
-yambar -c "$dir/yambar/config.yml" &
 
 # Set wallpaper
 if [[ $(pidof wbg) ]]; then
@@ -18,21 +26,17 @@ if [[ $(pidof wbg) ]]; then
 fi
 wbg "$dir/themes/nord/wallpaper" &
 
+# Lauch statusbar
+if [[ $(pidof yambar) ]]; then
+  pkill yambar
+fi
+yambar -c "$dir/yambar/config.yml" &
+
 # Launch dunst daemon
 if [[ $(pidof dunst) ]]; then
   pkill dunst
 fi
 dunst -config "$dir/dunstrc" &
-
-## Mount Google Drive
-if [[ ! $(pidof rclone) ]]; then
-  rclone mount --daemon GoogleDriveMain: "$HOME/Google Drive" &
-fi
-
-# Launch jamesdsp
-if [[ ! $(pidof jamesdsp) ]]; then
-  jamesdsp -t &
-fi
 
 # Launch foot server
 if [[ ! $(pidof foot) ]]; then
@@ -45,11 +49,12 @@ if [[ ! $(pidof swayidle) ]]; then
     timeout 300 "$dir/scripts/waylock.sh" &
 fi
 
-# Launch mpd with playerctl mpd-mpris plugin
-if [[ ! $(pidof mpd) ]]; then
-  mpd &
-  wait $!
-  mpd-mpris &
-elif [[ ! $(pidof mpd-mpris) ]]; then
-  mpd-mpris &
+## Mount Google Drive
+if [[ ! $(pidof rclone) ]]; then
+  rclone mount --daemon GoogleDriveMain: "$HOME/Google Drive" &
+fi
+
+# Launch jamesdsp
+if [[ ! $(pidof jamesdsp) ]]; then
+  jamesdsp -t &
 fi
